80671bd32a950a3826949d8c9033e324e83047ef
christiansingian <christian.singian@enduraplas.com>
2020-11-13 10:46:34 -0500

    First Upload
    First Upload

---
+++ main.js
@@ -0,0 +1,22 @@
+jQuery(document).ready(function($) {
+	$('.owl-carousel').owlCarousel({
+	    loop:true,
+	    margin:10,
+	    nav:true,
+	    dots: false,
+	    items: 2,
+	    margin: 30,
+	    autoplay:true,
+	})
+
+	$('.magic-box').matchHeight();
+	$('.attr-1').matchHeight();
+	$('.attr-2').matchHeight();
+	$('.attr-3').matchHeight();
+	$('.attr-4').matchHeight();
+	$('.attr-5').matchHeight();
+	$('.attr-6').matchHeight();
+	$('.attr-7').matchHeight();
+	$('.attr-8').matchHeight();
+	$('.attr-9').matchHeight();
+});
